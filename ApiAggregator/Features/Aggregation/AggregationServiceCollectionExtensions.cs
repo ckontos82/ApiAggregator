3c0f06ab26dd690b4e1176d26c964ba888c72631
harko <harko@DAEMONSLAPTOP>
2026-08-04 20:24:07 +0300

    Refactor aggregation service registration

---
+++ ApiAggregator/Features/Aggregation/AggregationServiceCollectionExtensions.cs
@@ -0,0 +1,59 @@
+using ApiAggregator.Features.Aggregation.Providers;
+using ApiAggregator.Features.Aggregation.Providers.GitHub;
+using ApiAggregator.Features.Aggregation.Providers.NewsApi;
+using ApiAggregator.Features.Aggregation.Services;
+using System.Net.Http.Headers;
+
+namespace ApiAggregator.Features.Aggregation;
+
+public static class AggregationServiceCollectionExtensions
+{
+    public static IServiceCollection AddAggregation(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(configuration);
+
+        AddGitHubProvider(services);
+        AddNewsApiProvider(services, configuration);
+
+        services.AddScoped<IAggregationProvider>(serviceProvider =>
+            serviceProvider.GetRequiredService<GitHubProvider>());
+        services.AddScoped<IAggregationProvider>(serviceProvider =>
+            serviceProvider.GetRequiredService<NewsApiProvider>());
+        services.AddScoped<IAggregationService, AggregationService>();
+
+        return services;
+    }
+
+    private static void AddGitHubProvider(IServiceCollection services)
+    {
+        services.AddHttpClient<GitHubProvider>(client =>
+        {
+            client.BaseAddress = new Uri("https://api.github.com/");
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("ApiAggregator/1.0");
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
+            client.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2026-03-10");
+            client.Timeout = TimeSpan.FromSeconds(15);
+        });
+    }
+
+    private static void AddNewsApiProvider(
+        IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var apiKey = configuration["ExternalApis:NewsApi:ApiKey"]
+            ?? throw new InvalidOperationException(
+                "NewsAPI API key is not configured.");
+
+        services.AddHttpClient<NewsApiProvider>(client =>
+        {
+            client.BaseAddress = new Uri("https://newsapi.org/v2/");
+            client.DefaultRequestHeaders.Add("X-Api-Key", apiKey);
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("ApiAggregator/1.0");
+            client.Timeout = TimeSpan.FromSeconds(15);
+        });
+    }
+}
--- ApiAggregator/Program.cs
+++ ApiAggregator/Program.cs
@@ -1,9 +1,5 @@
-using ApiAggregator.Features.Aggregation.Providers;
-using ApiAggregator.Features.Aggregation.Providers.GitHub;
-using ApiAggregator.Features.Aggregation.Providers.NewsApi;
-using ApiAggregator.Features.Aggregation.Services;
+using ApiAggregator.Features.Aggregation;
 using Scalar.AspNetCore;
-using System.Net.Http.Headers;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,36 +15,7 @@ builder.Services.AddControllers()
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddHttpClient<GitHubProvider>(client =>
-{
-    client.BaseAddress = new Uri("https://api.github.com/");
-    client.DefaultRequestHeaders.UserAgent.ParseAdd("ApiAggregator/1.0");
-    client.DefaultRequestHeaders.Accept.Add(
-        new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
-    client.DefaultRequestHeaders.Add("X-GitHub-Api-Version", "2026-03-10");
-    client.Timeout = TimeSpan.FromSeconds(15);
-});
-
-var newsApiKey =
-    builder.Configuration["ExternalApis:NewsApi:ApiKey"]
-    ?? throw new InvalidOperationException(
-        "NewsAPI API key is not configured.");
-
-builder.Services.AddHttpClient<NewsApiProvider>(client =>
-{
-    client.BaseAddress = new Uri("https://newsapi.org/v2/");
-    client.DefaultRequestHeaders.Add("X-Api-Key", newsApiKey);
-    client.DefaultRequestHeaders.UserAgent.ParseAdd("ApiAggregator/1.0");
-    client.Timeout = TimeSpan.FromSeconds(15);
-});
-
-builder.Services.AddScoped<IAggregationProvider>(
-    serviceProvider => serviceProvider.GetRequiredService<GitHubProvider>());
-
-builder.Services.AddScoped<IAggregationProvider>(
-    serviceProvider => serviceProvider.GetRequiredService<NewsApiProvider>());
-
-builder.Services.AddScoped<IAggregationService, AggregationService>();
+builder.Services.AddAggregation(builder.Configuration);
 
 var app = builder.Build();
 
